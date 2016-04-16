f682d6f4ee4585e7eaff3582e9ecf5f39bb23951
Oliviu Brates <gmforever8899@outlook.com>
2016-04-16 04:41:33 +0200

    added env variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: 'smtp.sendgrid.net',
       port: 587,
-      user_name: 'inspireops',
-      password: 'inspireops123'
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD']
   }
 end
