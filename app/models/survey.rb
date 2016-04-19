8d9538b82d2a08b37e9f45f52f0a72ae68a1cbda
Oliviu Brates <gmforever8899@outlook.com>
2016-04-19 05:12:05 +0200

    fixed survey model

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,7 +2,7 @@ class Survey < ActiveRecord::Base
   belongs_to :user
   has_one :question
   has_one :landing_page
-  has_many :responses, through: question
+  has_many :responses, through: :question
 
   validates :name, presence: true
 end
