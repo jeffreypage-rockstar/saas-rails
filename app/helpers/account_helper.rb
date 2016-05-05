d041a51b0f5240cbed490663892b2907beeca5b3
Oliviu Brates <gmforever8899@outlook.com>
2016-05-05 18:08:23 +0200

    subscription issue fixed.

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -2,4 +2,20 @@ module AccountHelper
   def account_active_class(link_path)
     current_page?(link_path) ? 'btn btn-default active' : 'btn btn-default'
   end
+
+  def subscribe_button_text(user, plan_name)
+    if user.stripe_detail.present? && user.stripe_detail.plan_id.present?
+      selected_plan = Plan.find_by_name(user.stripe_detail.plan_id)
+      plan = Plan.find_by_name(plan_name)
+      if selected_plan.id == plan.id
+        'Current Plan'
+      elsif selected_plan.id < plan.id
+        'Upgrade'
+      else
+        'Downgrade'
+      end
+    else
+      'Start Free Trial'
+    end
+  end
 end
