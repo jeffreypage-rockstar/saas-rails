610db649c8b80b63235a19ef2beceef84f9de338
Oliviu Brates <gmforever8899@outlook.com>
2016-05-14 04:27:40 +0200

    added google site verification html file.

--- config/routes.rb
+++ config/routes.rb
@@ -63,5 +63,8 @@ Rails.application.routes.draw do
     post '/webhook', to: 'subscriptions#webhook'
     post '/landing_page/preview', to: 'landing_pages#preview', as: 'landing_page_preview'
   end
+
+  match '/google2cb28f06c5aee4fe.html',
+        to: proc { |env| [200, {}, ["google-site-verification: google2cb28f06c5aee4fe.html"]] }
 end
 
